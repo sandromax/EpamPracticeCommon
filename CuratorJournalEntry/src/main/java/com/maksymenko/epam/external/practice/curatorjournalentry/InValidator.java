a3c1be795fdb460971b526f1e6f7b3c1df144324
sandromax <sandrondk@gmail.com>
2017-11-22 23:00:07 +0200

    created project: CuratorJournalEntry

---
+++ CuratorJournalEntry/src/main/java/com/maksymenko/epam/external/practice/curatorjournalentry/InValidator.java
@@ -0,0 +1,11 @@
+package com.maksymenko.epam.external.practice.curatorjournalentry;
+
+public class InValidator {
+
+    //фамилия студента
+    //имя студента
+    //дата рождения студента
+    //телефон студента
+    //адрес студента (улица, дом, квартира)
+
+}
